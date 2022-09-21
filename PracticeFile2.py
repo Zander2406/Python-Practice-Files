fcfe2a4c34b41c1b6e7e20e7eea3d8ce907ec906
Zander <swarnavabhattacharya24@gmail.com>
2022-09-21 14:58:12 +0530

    Second Day

---
+++ PracticeFile2.py
@@ -0,0 +1,92 @@
+
+"""
+Conditional Statements
+"""
+
+a = 24
+b = 30
+
+# if (a > b):
+#     print("A is greater")
+# elif (a == b):
+#     print("A is equal to B")
+# else:
+#     print("B is greater")
+
+
+# if (b > a):
+#     print("B is greater")
+#     print("B is executed")
+# if (a > b):
+#     print("A is greater")
+#     print("A is executed")
+
+teststring = "password"
+
+# if "pass" in teststring:
+#     print("If 1 Pass is present")
+# elif "passw" in teststring:
+#     print("If 2 Passw is present")
+
+""" <, >, ==, in, >=, <=, is """
+
+a = "pass"
+
+b = "pass"
+# if (a is b):
+#     print("Success")
+# else:
+#     print("Fail")
+
+
+astring = "Password"
+bstring = "Pass"
+
+# print(bstring in astring)
+
+
+
+
+# for i in range(0, 10):
+#     # print(i)
+#     # if (i == 5):
+#     #     break
+#     if (i == 5):
+#         continue
+#     print(i)
+# else:
+#     print("Loop ended")
+
+st = "password"
+llst = [1, 2, 3, "Ankit", 89]
+"       0, 1, 2,   3    , 4 "
+
+for item in st:
+    print(item, end=" ")
+print()
+for item in llst:
+    print(item, end=" ")
+print()
+# for i in range(len(llst)):
+#     print(llst[i], end=" ")
+
+for i in range(len(llst)):
+    print(llst[i], end=" ")
+print()
+i = 0
+# while i < 5:
+#     print(i, end=" ")
+#     i += 1
+# else:
+#     print()
+#     print("Loop ended")
+
+
+print("Test")
+print("\n")
+print("Test 2")
+
+
+
+
+
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,4 @@
-Ankit
+
+#     print("If 1 Pass is present")
+# elif "passw" in teststring:
+#     print("If 2 Passw is present")
