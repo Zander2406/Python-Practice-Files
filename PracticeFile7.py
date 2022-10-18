8023dc87397f45cdf85e98fc9402018ac8f945d4
Zander <swarnavabhattacharya24@gmail.com>
2022-10-18 13:43:04 +0530

    Day 8

---
+++ PracticeFile7.py
@@ -0,0 +1,97 @@
+# x = int(input("Enter x = "))
+# y = int(input("Enter y = "))
+# print(f"({x} + {y}) ^ 2 = {(x + y) ** 2}")
+
+
+
+# principal = float(input("Enter principle: "))
+# time = float(input("Enter time (in years): "))
+# rate = float(input("Enter rate (in %): "))
+# CI = principal * ((1 + (rate / 100)) ** time)
+# print(f"Amount is {round(CI, 2)}")
+
+
+
+
+
+
+# x_coordinates = input("Enter the coordinates (x1, x2...): ")
+# y_coordinates = input("Enter the coordinates (y1, y2...): ")
+# x_coordinates = x_coordinates.split(", ")
+# y_coordinates = y_coordinates.split(", ")
+# x_coordinates = [int(x) for x in x_coordinates]
+# y_coordinates = [int(y) for y in y_coordinates]
+# distance = ((x_coordinates[0] - x_coordinates[1])**2 + (y_coordinates[0] - y_coordinates[1])**2) ** 0.5
+# print(round(distance, 2))
+
+
+
+
+# test_array = [23, 56, 2378, 8945, 78]
+# for x in test_array:
+#     print(x, end=" ")
+
+
+import os
+f = os.path.basename(__file__)
+print(f)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
